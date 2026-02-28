9d7d2454b103c7e67486747c746edf5c15697470
nkalupahana <avishkank@gmail.com>
2026-02-27 23:59:58 -0500

    Basic checks

--- main/main.cpp
+++ main/main.cpp
@@ -1,8 +1,25 @@
 #include <Arduino.h>
 
+const uint8_t CHECK_PINS[] = {23};
+const uint8_t BUZZER_PIN = 14;
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Hello, World!");
+
+  for (uint8_t pin : CHECK_PINS) {
+    pinMode(pin, INPUT_PULLUP);
+  }
+  pinMode(BUZZER_PIN, OUTPUT);
 }
 
-void loop() {}
+void loop() {
+  bool open = false;
+  for (uint8_t pin : CHECK_PINS) {
+    if (digitalRead(pin) == LOW) {
+      open = true;
+    }
+  }
+
+  Serial.println(open);
+}
